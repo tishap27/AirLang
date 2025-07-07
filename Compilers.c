2764affa999dce2199855deef74dfad96fc13d6e
tishap27 <pate1416@algonquinlive.com>
2025-07-07 13:25:43 -0400

    Add: AX4 files updated them for AirLang, updated compiler.c and compiler.h to call main4Parser now

--- Compilers.c
+++ Compilers.c
@@ -99,6 +99,11 @@ airlang_intg main(int argc, char** argv) {
 	printf("%s%c%s" , "\n[Option '", PGM_SCN, " ':Starting SCANNER....]\n\n");
 		main3Scanner(argc, argv);
 	break;
+	case PGM_PSR:
+		printf("%s%c%s", "\n[Option '", PGM_PSR, " ':Starting PARSER....]\n\n");
+		main4Parser(argc, argv);
+		break;
+
 
 	default:
 		printf("%s%c%s%c%s%c%s%c%s%c%s", "OPTIONS:\n* [",
---
+++ Step4Parser.h
@@ -0,0 +1,160 @@
+/*
+************************************************************
+* COMPILERS COURSE - Algonquin College
+* Code version: Fall, 2024
+* Author: Tisha Patel
+* Professors: Paulo Sousa
+************************************************************
+#
+# ECHO "=---------------------------------------="
+# ECHO "|  COMPILERS - ALGONQUIN COLLEGE (S25)  |"
+# ECHO "=---------------------------------------="
+# ECHO "    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    "
+# ECHO "    @@                             @@    "
+# ECHO "    @@            __|__            @@    "
+# ECHO "    @@-------@--o--(_)--o--@-------@@    "
+# ECHO "    @@                             @@    "
+# ECHO "    @@        A I R L A N G        @@    "
+# ECHO "    @@                             @@    "
+# ECHO "    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    "
+# ECHO "                                         "
+# ECHO "[PARSER SCRIPT .........................]"
+# ECHO "                                         "
+*/
+
+
+/*
+************************************************************
+* File name: Parser.h
+* Compiler: MS Visual Studio 2022
+* Course: CST 8152 – Compilers, Lab Section: [011, 012]
+* Assignment: A32.
+* Date: May 01 2023
+* Professor: Paulo Sousa
+* Purpose: This file is the main header for Parser (.h)
+************************************************************
+*/
+
+#ifndef PARSER_H_
+#define PARSER_H_
+
+/* Inclusion section */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+#ifndef COMPILERS_H_
+#include "Compilers.h"
+#endif
+#ifndef READER_H_
+#include "Reader.h"
+#endif
+#ifndef SCANNER_H_
+#include "Scanner.h"
+#endif
+
+/* Global vars */
+static Token			lookahead;
+extern BufferPointer	stringLiteralTable;
+extern airlang_intg		line;
+extern Token			tokenizer(airlang_void);
+extern airlang_strg		keywordTable[KWT_SIZE];
+static airlang_intg		syntaxErrorNumber = 0;
+
+#define LANG_WRTE		"print&"
+#define LANG_READ		"input&"
+#define LANG_MAIN		"main&"
+
+/* TO_DO: Create ALL constants for keywords (sequence given in table.h) */
+
+/* Constants */
+enum KEYWORDS {
+	NO_ATTR = -1,
+	KW_data,
+	KW_code,
+	KW_int,
+	KW_real,
+	KW_string,
+	KW_if,
+	KW_then,
+	KW_else,
+	KW_while,
+	KW_do,
+	KW_return
+};
+
+/* TO_DO: Define the number of BNF rules */
+#define NUM_BNF_RULES 14
+
+/* Parser */
+typedef struct parserData {
+	airlang_intg parsHistogram[NUM_BNF_RULES];	/* Number of BNF Statements */
+} ParserData, * pParsData;
+
+/* Number of errors */
+airlang_intg numParserErrors;
+
+/* Scanner data */
+ParserData psData;
+
+/* Function definitions */
+airlang_void startParser();
+airlang_void matchToken(airlang_intg, airlang_intg);
+airlang_void syncErrorHandler(airlang_intg);
+airlang_void printError();
+airlang_void printBNFData(ParserData psData);
+
+/* List of BNF statements */
+enum BNF_RULES {
+	BNF_error,										/*  0: Error token */
+	BNF_codeSession,								/*  1 */
+	BNF_comment,									/*  2 */
+	BNF_dataSession,								/*  3 */
+	BNF_optVarListDeclarations,						/*  4 */
+	BNF_optionalStatements,							/*  5 */
+	BNF_outputStatement,							/*  6 */
+	BNF_outputVariableList,							/*  7 */
+	BNF_program,									/*  8 */
+	BNF_statement,									/*  9 */
+	BNF_statements,									/* 10 */
+	BNF_statementsPrime,							/* 11 */
+	BNF_optParams,									/* 12 */
+	BNF_returnStatement								/* 13 */	
+};
+
+
+/* TO_DO: Define the list of keywords */
+static airlang_strg BNFStrTable[NUM_BNF_RULES] = {
+	"BNF_error",
+	"BNF_codeSession",
+	"BNF_comment",
+	"BNF_dataSession",
+	"BNF_optVarListDeclarations",
+	"BNF_optionalStatements",
+	"BNF_outputStatement",
+	"BNF_outputVariableList",
+	"BNF_program",
+	"BNF_statement",
+	"BNF_statements",
+	"BNF_statementsPrime",
+	"BNF_optParams",
+	"BNF_returnStatement"
+};
+
+/* TO_DO: Place ALL non-terminal function declarations */
+airlang_void codeSession();
+airlang_void comment();
+airlang_void dataSession();
+airlang_void optVarListDeclarations();
+airlang_void optionalStatements();
+airlang_void outputStatement();
+airlang_void outputVariableList();
+airlang_void program();
+airlang_void statement();
+airlang_void statements();
+airlang_void statementsPrime();
+airlang_void optParams();
+airlang_void paramList();
+
+#endif
